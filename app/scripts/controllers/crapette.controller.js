f5c9da45678d80d7f9a50568ca48c347a605978d
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2016-02-17 16:32:19 +0100

    Importing remaining work on crapette

--- app/scripts/controllers/crapette.controller.js
+++ app/scripts/controllers/crapette.controller.js
@@ -2,6 +2,52 @@
 
 angular.module('cardsApp')
   .controller('CrapetteCtrl', function ($scope, cards) {
-  	$scope.deck = cards.createDeck();
+  	//$scope.deck = cards.createDeck();
   	$scope.cards = cards;
-  });
+
+  	$scope.game = {};
+
+  	// It's crapette time
+	var NSLOTS = 8, NSTACKS = cards.CARDTYPES.length * 2, SIZECRAPETTE = 14;
+
+	$scope.game.slots = [];
+	for(var i=0;i<NSLOTS; i++){
+		$scope.game.slots.push([]);
+	}
+
+	$scope.game.aceStacks = [];
+	for(var i=0;i<NSTACKS; i++){
+		$scope.game.aceStacks.push([]);
+	}
+
+	function initCrapettePlayer(playerId, _game){
+		var player = cards.createDeck();
+		for(var i=0; i < player.length; i++){
+			i.player = playerId;
+		}
+
+		var playerDeck = cards.copyDeckDeep(player).shuffle();
+		var playerCrapette = [];
+
+		// Fill crapette
+		for(var i = 0; i < SIZECRAPETTE; i++){
+			playerCrapette.push(playerDeck.pop());
+		}
+		// Fill slots
+		for(var i=playerId*NSLOTS/2; i<(playerId+1)*NSLOTS/2; i++){
+			_game.slots[i].push(playerDeck.pop());
+		}
+
+		return {
+			id: playerId,
+			refs: player,
+			deck: playerDeck,
+			crapette: playerCrapette,
+			discard: []
+		}
+	}
+
+	$scope.game.players = [];
+	$scope.game.players.push(initCrapettePlayer(0, $scope.game));
+	$scope.game.players.push(initCrapettePlayer(1, $scope.game));
+});
